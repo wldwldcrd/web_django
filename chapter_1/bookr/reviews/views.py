fe6e5b7b94d2dd492d8af7120e5a9616a426a50d
wldwldcrd <wldwldcrd@gmail.com>
2024-07-11 18:59:54 +0300

    Exercise 3.03

--- chapter_1/bookr/reviews/views.py
+++ chapter_1/bookr/reviews/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 from .models import Book
+from .utils import average_rating
 
 
 def index(request):
-    name = "world"
-    return render(request, "base.html",
-                  {"name": name})
+    return render(request, "base.html")
 
 
 def book_search(request):
     search_text = request.GET.get("search", "")
-    return render(request, "search-results.html",
+    return render(request,
+                  "reviews/search-results.html",
                   {"search_text": search_text})
 
 
-def welcome_view(request):
-    return render(request, 'base.html')
+def book_list(request):
+    books = Book.objects.all()
+    book_list = []
+    for book in books:
+        reviews = book.review_set.all()
+        if reviews:
+            book_rating = average_rating([review.rating for review in reviews])
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        book_list.append({'book': book,
+                          'book_rating': book_rating,
+                          'number_of_reviews': number_of_reviews})
+
+    context = {
+        'book_list': book_list
+    }
+    return render(request, 'reviews/book_list.html', context)
